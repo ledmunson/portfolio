91de0bce8d92110562993cf90e187e451306f43d
Leo <ledmunson90@gmail.com>
2018-04-15 13:38:10 -0700

    Comic Book MVC Site
    This is a MVC web application I created for comic books.

---
+++ ComicBookMVC/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstChallenge.Models;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comicBooks = ComicBookManager.GetComicBooks();
+            return View(comicBooks);
+        }
+        
+        /*
+        public ActionResult Comic41()
+        {
+            var comicBooks = ComicBookManager.GetComicBooks();
+            return View(comicBooks);
+        }
+
+        public ActionResult Comic13()
+        {
+            var comicBooks = ComicBookManager.GetComicBooks();
+            return View(comicBooks.ElementAt(1));
+        }
+
+        public ActionResult Comic17()
+        {
+            var comicBooks = ComicBookManager.GetComicBooks();
+            return View(comicBooks.ElementAt(2));
+        }
+        */
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comic);
+        }
+    }
+}
